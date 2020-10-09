b03b2fb86e92fef6d1a1dada9a575603cf921eaf
Adarsh Raj Baronia <37274176+adarshbaronia@users.noreply.github.com>
2020-10-09 10:00:26 +0530

    Update password generator.py

--- password generator.py
+++ password generator.py
@@ -13,6 +13,35 @@ def password_generator(n):
     special_choice=''.join(random.choices(special,k=math.ceil(n/4)))
     
     password=''.join(random.sample(small_choice + upper_choice + num_choice + special_choice, k=n)) 
+    #print(f"The genrated password of length {n}: ",password)
     return password
 
-print(password_generator(10))
+
+#decode
+
+def password_decode():
+    password=password_generator(int(input("Please enter password length: ")))
+    print(f"The password of length {len(password)} converted in asterick",len(password)*"*")
+    import string
+    count=0
+    decode=''
+    for i in range(len(password)):
+        if password[i] in string.ascii_letters:
+            decode=decode+password[i]
+            i+=1
+            count+=1
+        elif password[i] in string.digits:
+            decode=decode+password[i]
+            i+=1
+            count+=1
+        elif password[i] in string.punctuation:
+            decode=decode+password[i]
+            i+=1
+            count+=1
+        else:
+            count+=1        
+    print(f"The decoding of generated password took {count} iterations")
+    return decode
+
+print("Decoded password is: ",password_decode())
+
